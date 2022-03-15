430389808a2ae5144e207989a59bada68e815c90
Fnr <ah2014bh@outlook.com>
2022-03-15 07:52:39 +0300

    fix register global slash comands

--- src/deploy/register.js
+++ src/deploy/register.js
@@ -10,8 +10,8 @@ const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 	try {
 		console.log('[Discord API] Started refreshing application (/) commands.'.yellow);
 		await rest.put(
-			process.env.DEVELOPMENT ? Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID)
-      : Routes.applicationCommands(process.env.CLIENT_ID),
+			process.env.DEVELOPMENT === true ? Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID)
+      		: Routes.applicationCommands(process.env.CLIENT_ID),
 			{ body: commands },
 		);
 		console.log('[Discord API] Successfully reloaded application (/) commands.'.green);
